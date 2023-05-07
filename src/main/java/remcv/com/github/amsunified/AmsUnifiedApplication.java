739a267be64e540942f5ff15ff9132282bbc8a62
remcv <remuluscatana@tutanota.com>
2023-05-07 21:39:29 +0300

    Initial Spring Initilizr commit

---
+++ src/main/java/remcv/com/github/amsunified/AmsUnifiedApplication.java
@@ -0,0 +1,13 @@
+package remcv.com.github.amsunified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsUnifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmsUnifiedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/remcv/com/github/amsunified/AmsUnifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package remcv.com.github.amsunified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmsUnifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
